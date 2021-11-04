233f94bac0daab8f941323d1b0c0d22baaf4c06b
Thatiely Stober Appelt <thatiely.appelt@fortune.local>
2021-11-04 16:13:05 -0300

    feat:

--- to-do-list/src/app/todo/item/item.component.ts
+++ to-do-list/src/app/todo/item/item.component.ts
@@ -1,4 +1,3 @@
-import { createNgModuleType } from '@angular/compiler/src/render3/r3_module_compiler';
 import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
@@ -10,38 +9,27 @@ import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
 export class ItemComponent implements OnInit {
   constructor() { }
   @Input() itemObj: any;
-  // @Output() checked = new EventEmitter;
 
   ngOnInit(): void {
     
   }
 
   checkedItem(e: any){
-    // console.log(e.target.checked); // true/false
-    // console.log(this.itemObj)
-    var teste = this.itemObj;
-    
-    if(e.target.checked === true){
-      const ls = localStorage.getItem('item') || '[]';
-      const list = JSON.parse(ls);
-      
-      list.forEach(function(e: any){
-        if(e.name == teste.name)
-        {
-          console.log(teste);
-          localStorage.removeItem('item');
-        }else{
-          //console.log("false");
-        }
-        
-      });
+    const itemCheck = this.itemObj;
+    const checked = e.target.checked;
 
+    const ls = localStorage.getItem('item') || '[]';
+    const list = JSON.parse(ls);
 
+    list.forEach((e: any, index: any) => {
+      if(e.name == itemCheck.name){
+        // console.log(itemCheck);
+        // console.log(index);
 
-    }else{
+        list.splice(index, 1, {name: itemCheck.name, check: checked});
+        console.log(list);
 
-    }
+      }
+    });
   }
-
-  
 } 
